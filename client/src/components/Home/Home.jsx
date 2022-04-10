b5a3682e48f2f22f35ae5733dd426605aec37cb3
tyrus <tyrushenry03@gmail.com>
2022-04-10 17:47:51 -0400

    file structure setup

---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import './Home.css'
+import { NavLink } from 'react-router-dom'
+
+const Home = () => {
+  return (
+  <div>
+  
+  </div>
+  )
+}
+
+export default Home;
---
+++ client/src/containers/MainContainer.jsx
@@ -0,0 +1,11 @@
+import { Routes } from "react-router-dom";
+
+export default function MainContainer() {
+  return (
+    <div>
+      <Routes>
+        
+      </Routes>
+    </div>
+  )
+}
