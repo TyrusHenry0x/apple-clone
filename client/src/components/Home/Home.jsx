0e6bb77032ae83316789ff1d22edf3a568117264
tyrus <tyrushenry03@gmail.com>
2022-04-30 14:58:38 -0400

    current

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -21,28 +21,53 @@ const Home = () => {
           <a href='' className='learn-more-link'>Learn more </a>
           <a href='' className='buy-link'>Buy</a>
         </div>
-        <img className='iphone-se-image' src='/assets/images/Apple-iPhoneSE-double-infinity-220308_Full-Bleed-Image.jpg.slideshow-xlarge.png'/>
+        <img className='iphone-se-image' src='/assets/images/Apple-iPhoneSE-double-infinity-220308_Full-Bleed-Image.jpg.slideshow-xlarge.png' />
       </div>
 
       <div className='ipad-air-div'>
-        <h1 className='ipad-air-h1'>iPad air</h1>
-        <p className='ipad-air-p'>Light. Bright. Full of might.</p>
-        <a href='' className='learn-more-link'>Learn more </a>
-        <a href='' className='buy-link'>Buy</a>
-        <div className='ipad-air-image'></div>
+        <div className='ipad-text-div'>
+          <div className='h1-div'>
+            <h1 className='ipad-air-h1'>iPad</h1>
+            <img className='air-icon' src='assets/images/IPad_Air_(4th_generation,_logo).svg.png' />
+          </div>
+          <p className='ipad-air-p'>Light. Bright. Full of might.</p>
+          <a href='' className='learn-more-link'>Learn more </a>
+          <a href='' className='buy-link'>Buy</a>
+        </div>
+        <img className='ipad-air-image' src='/assets/images/ipad-air_overview__cahgij9otxua_og.png' />
       </div>
 
       <div className='advert-grid'>
-        <div className='wwdc22-image'>
-          <a href='' className='wwdc22-link'>Learn more</a>
+
+        <div className='wwdc22-div'
+        // style={{ background: `url('/assets/images/wwwdc-2022.webp')` }}
+        >
+          <img className='wwdc22-image' src='/assets/images/wwdc22-og.jpeg' />
+          <div className='wwdc22-h1-div'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="wwdc22-apple-icon" viewBox="0 0 16 16">
+              <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282z" />
+              <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282z" />
+            </svg>
+            <h1 className='wwdc22-h1'>WWDC22</h1>
+          </div>
+          <p className='wwdc22-p'>The Worldwide Developers Conference is coming. Join us online June 6-10.</p>
+          <a href='' className='grid-learn-more-link'>Learn more</a>
         </div>
 
-        <div className='apple-watch-box'>
-          <h1>WATCH</h1>
-          <p className='apple-watch-p'>SERIES 7</p>
-          <a href='' className='grid-learn-more-link'>Learn more</a>
-          <a href='' className='grid-buy-link'>Buy</a>
-          <div className='apple-watch-image'></div>
+        <div className='apple-watch-div'>
+          <div className='apple-watch-h1-div'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="watch-apple-icon" viewBox="0 0 16 16">
+              <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282z" />
+              <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282z" />
+            </svg>
+            <h1 className='apple-watch-h1'>WATCH</h1>
+          </div>
+          <p className='apple-watch-sub'>SERIES 7</p>
+          <div className='grid-link-div'>
+            <a href='' className='grid-learn-more-link'>Learn more</a>
+            <a href='' className='grid-buy-link'>Buy</a>
+          </div>
+          <img className='apple-watch-image' src='/assets/images/apple-watch.png'></img>
         </div>
 
         <div className='studio-display-box'>
