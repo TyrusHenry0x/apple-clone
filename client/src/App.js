994a554c1f0bebe544756cebfab3fb227fa69002
tyrus <tyrushenry03@gmail.com>
2022-06-17 12:40:21 -0400

    navbar finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
+import Nav from './components/Nav/Nav';
+
 function App() {
   return (
     <div className="App">
+      <Nav/>
       <Routes>
       <Route exact path="/" element={<Home />} />
       </Routes>
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -89,22 +89,41 @@ const Home = () => {
         <div className='mac-studio-div'>
           <h1 className='mac-studio-h1'>Mac Studio</h1>
           <p className='mac-studio-p'>Empower station.</p>
-          <a href='' className='grid-learn-more-link'>Learn more</a>
-          <a href='' className='grid-buy-link'>Buy</a>
-          <div className='mac-studio-image'></div>
+          <div className='grid-link-div'>
+            <a href='' className='grid-learn-more-link'>Learn more</a>
+            <a href='' className='grid-buy-link'>Buy</a>
+          </div>
+          <img className='mac-studio-image' src='/assets/images/mac-studio-og-202203.png'></img>
         </div>
 
         <div className='apple-arcade-div'>
-          <h1 className='apple-arcade-h1'>Arcade</h1>
-          <p className='apple-arcade-p'>Gear.Club Stradale. Play now.</p>
-          <a href='' className='grid-learn-more-link'>Learn more</a>
-          <a href='' className='grid-buy-link'>Try it free¹</a>
+          <div className='arcade-h1-div'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="arcade-icon" viewBox="0 0 16 16">
+              <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282z" />
+              <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282z" />
+            </svg>
+            <h1 className='arcade-h1'>Arcade</h1>
+          </div>
+          <p className='arcade-p'>Play extraordinary</p>
+          <div className='grid-link-div'>
+            <a href='' className='grid-learn-more-link'>Learn more</a>
+            <a href='' className='grid-buy-link'>Try it free¹</a>
+          </div>
+          <img className='arcade-img' src='/assets/images/Apple_apple-arcade_the-enchanted-world-1_091619_big_carousel.jpg.large.png' />
         </div>
 
         <div className='gutter'>
         </div>
 
-        <div className='apple-tv-image'>
+        <div className='apple-tv-image'
+          style={{
+            width: '100%',
+            height: 465,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            background: `url('${process.env.PUBLIC_URL}/assets/images/severance-apple-tv-plus.jpeg')`
+          }}
+        >
           <a href='' className='apple-tv-link'>Stream all episodes now »</a>
         </div>
       </div>
