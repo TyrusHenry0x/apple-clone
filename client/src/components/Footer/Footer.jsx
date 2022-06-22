5c3a1209f98574963c8f12dfc693b619b1d1f52a
tyrus <tyrushenry03@gmail.com>
2022-06-21 21:53:12 -0400

    footer complete

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,192 @@
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer-content'>
+
+
+      <ul className='footnote-1'>
+        <li className='footnote-content'><span >1. $4.99/month after free trial. One subscription per Family&nbsp;Sharing group. Offer good for 3 months after eligible device activation. Plan automatically renews until cancelled. Restrictions and other <a href='javascript:void(0);' className='footer-link'>terms</a>&nbsp;apply.</span>
+        </li>
+        <li className='footnote-content'>To access and use all the features of Apple&nbsp;Card, you must add Apple&nbsp;Card to Wallet on an iPhone or iPad with the latest version of iOS or iPadOS. Update to the latest version by going to Settings &gt; General &gt; Software Update. Tap Download and&nbsp;Install.</li>
+        <li className='footnote-content'>Available for qualifying applicants in the United&nbsp;States.</li>
+        <li className='footnote-content'>Apple&nbsp;Card is issued by Goldman Sachs Bank USA, Salt&nbsp;Lake&nbsp;City&nbsp;Branch.</li>
+        <li className='footnote-content'>Learn more about how Apple&nbsp;Card applications are evaluated at <a href='javascript:void(0);' className='footer-link'>support.apple.com/kb/HT209218</a>.</li>
+        <li className='footnote-content'>$4.99/month after free trial. Only one offer per Apple&nbsp;ID and only one offer per family if you’re part of a Family&nbsp;Sharing group, regardless of the number of devices you or your family purchases. This offer is not available if you or your Family have previously accepted an Apple&nbsp;TV+ one year free offer. Offer good for 3 months after eligible device activation. Plan automatically renews until cancelled. Restrictions and other <a href='javascript:void(0);' className='footer-link'>terms</a>&nbsp;apply.
+        </li>
+      </ul>
+
+      <div>
+        <nav className='footer-columns'>
+
+          <div className='footer-directory-column'>
+            <div className='footer-directory-column-section'>
+              <label className='footer-directory-column-section-label'>
+                <h3 className='footer-column-section-title'>Shop and Learn</h3>
+              </label>
+              <ul className='footer-directory-column-section-list'>
+                <li className='footer-directory-column-section-item'>Store</li>
+                <li className='footer-directory-column-section-item'>Mac</li>
+                <li className='footer-directory-column-section-item'>iPad</li>
+                <li className='footer-directory-column-section-item'>iPhone</li>
+                <li className='footer-directory-column-section-item'>Watch</li>
+                <li className='footer-directory-column-section-item'>AirPods</li>
+                <li className='footer-directory-column-section-item'>TV & Home</li>
+                <li className='footer-directory-column-section-item'>AirTag</li>
+                <li className='footer-directory-column-section-item'>Accessories</li>
+                <li className='footer-directory-column-section-item'>Gift Cards</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className='footer-directory-column'>
+            <div className='footer-directory-column-section'>
+              <label className='footer-directory-column-section-label'>
+                <h3 className='footer-column-section-title'>Services</h3>
+              </label>
+              <ul className='footer-directory-column-section-list'>
+                <li className='footer-directory-column-section-item'>Apple Music</li>
+                <li className='footer-directory-column-section-item'>Apple TV+</li>
+                <li className='footer-directory-column-section-item'>Apple Fitness+</li>
+                <li className='footer-directory-column-section-item'>Apple News+</li>
+                <li className='footer-directory-column-section-item'>Apple Arcade</li>
+                <li className='footer-directory-column-section-item'>iCloud</li>
+                <li className='footer-directory-column-section-item'>Apple One</li>
+                <li className='footer-directory-column-section-item'>Apple Card</li>
+                <li className='footer-directory-column-section-item'>Apple Books</li>
+                <li className='footer-directory-column-section-item'>Apple Podcasts</li>
+                <li className='footer-directory-column-section-item'>App Store</li>
+              </ul>
+              <label className='footer-directory-column-section'>
+                <h3 className='footer-column-section-title'>Account</h3>
+              </label>
+              <ul className='footer-directory-column-section-list'>
+                <li className='footer-directory-column-section-item'>Manage Your Apple ID</li>
+                <li className='footer-directory-column-section-item'>Apple
+                  Store Account</li>
+                <li className='footer-directory-column-section-item'>iCloud.com</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className='footer-directory-column'>
+            <div className='footer-directory-column-section'>
+              <label className='footer-directory-column-section-label'>
+                <h3 className='footer-column-section-title'>Apple Store</h3>
+              </label>
+              <ul className='footer-directory-column-section-list'>
+                <li className='footer-directory-column-section-item'>Find a Store</li>
+                <li className='footer-directory-column-section-item'>Genius Bar</li>
+                <li className='footer-directory-column-section-item'>Today at Apple</li>
+                <li className='footer-directory-column-section-item'>Apple Camp</li>
+                <li className='footer-directory-column-section-item'>Apple Store App</li>
+                <li className='footer-directory-column-section-item'>Refurbished and Clearance</li>
+                <li className='footer-directory-column-section-item'>Financing</li>
+                <li className='footer-directory-column-section-item'>Apple Trade In</li>
+                <li className='footer-directory-column-section-item'>Order Status</li>
+                <li className='footer-directory-column-section-item'>Shopping Help</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className='footer-directory-column'>
+            <div className='footer-directory-column-section'>
+              <label className='footer-directory-column-section-label'>
+                <h3 className='footer-column-section-title'>For Business</h3>
+              </label>
+              <ul className='footer-directory-column-section-list'>
+                <li className='footer-directory-column-section-item'>Apple and Business</li>
+                <li className='footer-directory-column-section-item'>Shop for Business</li>
+              </ul>
+
+              <label className='footer-directory-column-section-label'>
+                <h3 className='footer-column-section-title'>For Education</h3>
+              </label>
+              <ul className='footer-directory-column-section-list'>
+                <li className='footer-directory-column-section-item'>Apple and Education</li>
+                <li className='footer-directory-column-section-item'>Shop for K-12</li>
+                <li className='footer-directory-column-section-item'>Shop for College</li>
+              </ul>
+
+              <label className='footer-directory-column-section-label'>
+                <h3 className='footer-column-section-title'>For Healthcare</h3>
+              </label>
+              <ul className='footer-directory-column-section-list'>
+                <li className='footer-directory-column-section-item'>Apple in Healthcare</li>
+                <li className='footer-directory-column-section-item'>Health on Apple Watch</li>
+                <li className='footer-directory-column-section-item'>Health Records on iPhone</li>
+              </ul>
+
+              <label className='footer-directory-column-section-label'>
+                <h3 className='footer-column-section-title'>For Government</h3>
+              </label>
+              <ul className='footer-directory-column-section-list'>
+                <li className='footer-directory-column-section-item'>Shop for Government</li>
+                <li className='footer-directory-column-section-item'>Shop for Veterans and Military</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className='footer-directory-column'>
+            <div className='footer-directory-column-section'>
+              <label className='footer-directory-column-section-label'>
+                <h3 className='footer-column-section-title'>Apple Values</h3>
+              </label>
+              <ul className='footer-directory-column-section-list'>
+                <li className='footer-directory-column-section-item'>Accessibility</li>
+                <li className='footer-directory-column-section-item'>Education</li>
+                <li className='footer-directory-column-section-item'>Environment</li>
+                <li className='footer-directory-column-section-item'>Inclusion and Diversity</li>
+                <li className='footer-directory-column-section-item'>Privacy</li>
+                <li className='footer-directory-column-section-item'>Racial Equity and Justice</li>
+                <li className='footer-directory-column-section-item'>Supplier Responsibility</li>
+              </ul>
+            </div>
+
+            <div className='footer-directory-column-section'>
+              <label className='footer-directory-column-section-label'>
+                <h3 className='footer-column-section-title'>About Apple</h3>
+              </label>
+              <ul className='footer-directory-column-section-list'>
+                <li className='footer-directory-column-section-item'>Newsroom</li>
+                <li className='footer-directory-column-section-item'>Apple Leadership</li>
+                <li className='footer-directory-column-section-item'>Career Opportunities</li>
+                <li className='footer-directory-column-section-item'>Investors</li>
+                <li className='footer-directory-column-section-item'>Ethics & Compliance</li>
+                <li className='footer-directory-column-section-item'>Events</li>
+                <li className='footer-directory-column-section-item'>Contact Apple</li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <section className="bottom-footer">
+          <div className="bottom-footer-shop" x-ms-format-detection="none">
+            More ways to shop: <a href="javascript:void(0);" data-analytics-title="find an apple store" className='shop-link'>Find an Apple Store</a> or <a href="javascript:void(0);" data-analytics-title="other retailers or resellers" data-analytics-exit-link="" className='shop-link'>other retailer</a> near you. <span className="nowrap">Or call 1-800-MY-APPLE.</span>
+          </div>
+          <div className='footer-flex'>
+            <div className="footer-legal">
+              <div className="footer-legal-copyright">Copyright ©
+
+                2022
+                Apple Inc. All rights reserved.
+              </div>
+              <div className="footer-legal-links">
+                <a className="footer-legal-link" href="" data-analytics-title="privacy policy">Privacy Policy</a>
+                <a className="footer-legal-link" href="" data-analytics-title="terms of use">Terms of Use</a>
+                <a className="footer-legal-link" href="" data-analytics-title="sales and refunds">Sales and Refunds</a>
+                <a className="footer-legal-link" href="" data-analytics-title="legal">Legal</a>
+                <a className="footer-legal-link" href="" tabindex="0" data-analytics-title="site map">Site Map</a>
+              </div>
+            </div>
+            <div className="footer-locale">
+              <a className="footer-locale-link" href="" title="Choose your country or region" aria-label="United States. Choose your country or region" data-analytics-title="choose your country">United States</a>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
